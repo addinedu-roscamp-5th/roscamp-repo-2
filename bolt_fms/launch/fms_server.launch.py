51999edcc698e6bd65d76f91c5cfa46354947375
gyu <kdjin11@naver.com>
2025-07-15 21:50:10 +0900

    Rename fms_system to bolt_fms

--- bolt_fms/launch/fms_server.launch.py
+++ bolt_fms/launch/fms_server.launch.py
@@ -4,27 +4,27 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='fms_system',
+            package='bolt_fms',
             executable='agent_manager_node',
             name='agent_manager'
         ),
         Node(
-            package='fms_system',
+            package='bolt_fms',
             executable='task_manager_node',
             name='task_manager'
         ),
         Node(
-            package='fms_system',
+            package='bolt_fms',
             executable='inventory_manager_node',
             name='inventory_manager'
         ),
         Node(
-            package='fms_system',
+            package='bolt_fms',
             executable='traffic_manager_node',
             name='traffic_manager'
         ),
         Node(
-            package='fms_system',
+            package='bolt_fms',
             executable='monitoring_manager_node',
             name='monitoring_manager'
         )
